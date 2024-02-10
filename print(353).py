95410ac356218ea8d3843235461a4775f1baee71
Dayncod <132489003+Dayncod@users.noreply.github.com>
2024-02-11 00:51:09 +0300

    Update print(353).py

--- print(353).py
+++ print(353).py
@@ -2,3 +2,11 @@ import telebot
 
 bot = telebot.TeleBot("6366348527:AAFcFWXB57aK06gZFdRn-Bdc0YNVcijb0C0")
 
+@bot.message_handler(content_types=['text'])
+def get_text_messages(message):
+    if message.text == "/start":
+        bot.send_message(message.from_user.id, "Привет")
+    else:
+        bot.send_message(message.from_user.id, "Мой разраб ещё работает над этой функцией")
+
+bot.infinity_polling()
